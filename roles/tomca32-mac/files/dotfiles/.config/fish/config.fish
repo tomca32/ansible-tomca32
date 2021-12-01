974bca0dbcee26026c75dd2478169335626e9b8f
Tomislav Pesut <tomca32@gmail.com>
2021-12-01 08:28:46 -0700

    Install Fisher and Fisher nvm

--- roles/tomca32-mac/files/dotfiles/.config/fish/config.fish
+++ roles/tomca32-mac/files/dotfiles/.config/fish/config.fish
@@ -15,10 +15,6 @@ alias intel="arch -x86_64"
 
 export ANDROID_HVPROTO=ddm # Android debugging
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
 echo "rvm default" # Activate RVM
 
 starship init fish | source
