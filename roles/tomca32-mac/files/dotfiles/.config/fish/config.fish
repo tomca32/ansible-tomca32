c2dc5db06d6cbdb7bab9eca4944953f549f6fc6b
Tomislav Pesut <tomca32@gmail.com>
2023-04-11 12:07:16 -0700

    Fix fish config

--- roles/tomca32-mac/files/dotfiles/.config/fish/config.fish
+++ roles/tomca32-mac/files/dotfiles/.config/fish/config.fish
@@ -1,8 +1,8 @@
 set PATH /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Users/tomislavpesut/bin
 set PATH $PATH /Applications/Visual Studio Code.app/Contents/Resources/app/bin # Add VS Code to Path
-set PATH $PATH $HOME/.cargo/bin:$PATH # Rust Package Manager - Cargo
+set PATH $PATH $HOME/.cargo/bin # Rust Package Manager - Cargo
 set PATH $PATH $HOME/.rvm/bin # Add RVM to PATH for scripting
-set PATH $PATH /Library/Frameworks/Mono.framework/Home/bin
+set PATH $PATH $HOME/.local/bin
 
 alias l="ls -al"
 alias g="git"
