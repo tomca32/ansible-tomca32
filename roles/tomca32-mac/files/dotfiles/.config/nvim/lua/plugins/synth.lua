e2084590e0383938d76f6b3c242c058b8919ab03
Tomislav Pesut <tomca32@gmail.com>
2023-08-19 20:06:03 +0200

    added synthwave theme for neovim

---
+++ roles/tomca32-mac/files/dotfiles/.config/nvim/lua/plugins/synth.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "lunarvim/synthwave84.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "synthwave84",
+    },
+  },
+}
