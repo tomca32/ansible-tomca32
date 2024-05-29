594a810bfe3e852eb4a2fab0474556b2cf5c13d3
Tomislav Pesut <tomca32@gmail.com>
2024-05-28 17:54:15 -0700

    Added formatting in nvim

--- roles/tomca32-mac/files/dotfiles/.config/nvim/lua/tomca32/plugins/lsp/mason.lua
+++ roles/tomca32-mac/files/dotfiles/.config/nvim/lua/tomca32/plugins/lsp/mason.lua
@@ -1,38 +1,50 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim"
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup()
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "typos_lsp",
-        "clangd",
-        "bashls",
-        "cssls",
-        "dockerls",
-        "docker_compose_language_service",
-        "html",
-        "htmx",
-        "tsserver",
-        "jinja_lsp",
-        "jqls",
-        "kotlin_language_server",
-        "lua_ls",
-        "autotools_ls",
-        "markdown_oxide",
-        "pyre",
-        "rust_analyzer",
-        "sqlls",
-        "stimulus_ls",
-        "taplo",
-        "tailwindcss",
-        "yamlls",
-      }
-    })
-  end
+		mason.setup()
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"typos_lsp",
+				"clangd",
+				"bashls",
+				"cssls",
+				"dockerls",
+				"docker_compose_language_service",
+				"html",
+				"htmx",
+				"tsserver",
+				"jinja_lsp",
+				"jqls",
+				"kotlin_language_server",
+				"lua_ls",
+				"autotools_ls",
+				"markdown_oxide",
+				"pyre",
+				"rust_analyzer",
+				"sqlls",
+				"stimulus_ls",
+				"taplo",
+				"tailwindcss",
+				"yamlls",
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"isort",
+				"black",
+				"rustfmt",
+			},
+		})
+	end,
 }
