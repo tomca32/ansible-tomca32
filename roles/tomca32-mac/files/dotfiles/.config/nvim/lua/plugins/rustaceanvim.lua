3fb4ca6e4b2a8eab63a06fa0fe5b3c9f9c31116e
Tomislav Pesut <tomca32@gmail.com>
2024-12-19 18:22:13 -0700

    Astronvim setup

---
+++ roles/tomca32-mac/files/dotfiles/.config/nvim/lua/plugins/rustaceanvim.lua
@@ -0,0 +1,7 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  'mrcjkb/rustaceanvim',
+  version = '^5', -- Recommended
+  lazy = false, -- This plugin is already lazy
+}
