64d71931e12741287fb5ebc496adcb6dc3fe3dc7
Tomislav Pesut <tomca32@gmail.com>
2024-12-24 11:07:42 -0700

    Setup neotest and inlay hints for neovim

--- roles/tomca32-mac/files/dotfiles/.config/nvim/lua/plugins/rustaceanvim.lua
+++
@@ -1,7 +0,0 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
-return {
-  'mrcjkb/rustaceanvim',
-  version = '^5', -- Recommended
-  lazy = false, -- This plugin is already lazy
-}
