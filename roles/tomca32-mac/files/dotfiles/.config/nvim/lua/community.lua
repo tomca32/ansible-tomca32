65cdbe0f09f23bef58f011d7ad70a548666fe6c4
Tomislav Pesut <tomca32@gmail.com>
2024-12-29 10:47:35 -0700

    Add lazygit and satellite plugin for nvim

--- roles/tomca32-mac/files/dotfiles/.config/nvim/lua/community.lua
+++ roles/tomca32-mac/files/dotfiles/.config/nvim/lua/community.lua
@@ -9,5 +9,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.test.neotest" }
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.scrolling.satellite-nvim" },
 }
